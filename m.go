1929f89b311de0e1a7b602c9463334b120735e7d
Kirill Bogatikov <kirillbogatikoff@gmail.com>
2023-03-27 00:43:26 +0300

    b

--- m.go
+++ m.go
@@ -1,11 +1,13 @@
 package m
 
 import (
+  "github.com/KirillBogatikov/m/a"
+  "github.com/KirillBogatikov/m/b"
   "github.com/KirillBogatikov/m/c"
 )
 
 func CallAll() {
-  for _, fn := range []func() { c.CallC } {
+  for _, fn := range []func() { a.CallA, b.CallB, c.CallC } {
     fn()
   }
 }
