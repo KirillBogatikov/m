4d2f2031e62cf6bbf13f7dfb5a9a03c013b6d4f8
Kirill Bogatikov <kirillbogatikoff@gmail.com>
2023-03-27 00:42:07 +0300

    update get.sh

--- get.sh
+++ get.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 version=${1}
-cd "$GOPATH/pkg/mod/github.com/!kirill!bogatikov/m@${version}"
-git init
-git remote add origin https://github.com/KirillBogatikov/m.git
-git branch -m main
-find ./ -not -name '.git' -print0 | xargs -0 rm --
-git pull origin main
-git submodule update --init --recursive
+dir="$GOPATH/pkg/mod/github.com/!kirill!bogatikov/m@${version}"
+
+rm -rf ${dir}
+mkdir ${dir} && cd ${dir} && \
+git init && \
+git remote add origin https://github.com/KirillBogatikov/m.git && \
+git branch -m main && \
+git pull origin main && \
+git submodule update --init --recursive && \
+find ./ -name '.git' -print0 | xargs -0 rm -rf -- && \
+find ./ -name '.gitmodules' -print0 | xargs -0 rm -rf -- && \
+find ./**/* -name 'go.*' -print0 | xargs -0 rm -rf -- && \
+rm -rf get.sh
